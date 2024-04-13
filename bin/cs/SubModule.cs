c2dfc16a0134573eedbe97d76fa55d501260d597
Alkoon-mtz <koonmtz@gmail.com>
2024-04-14 01:13:25 +0200

    feat: Add crusader and cereann troops
    Also push cs files to github

---
+++ bin/cs/SubModule.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem.CharacterCreationContent;
+using System.Reflection;
+
+namespace Split
+{
+    public class SubModule : MBSubModuleBase
+    {
+        private const string YOUR_MOD_PATCH_NAME_MAKE_IT_UNIQUE_PLZ = "SplitFrenchPatch";
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+            var harmony = new Harmony(YOUR_MOD_PATCH_NAME_MAKE_IT_UNIQUE_PLZ);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    internal class SplitFrenchPatch
+    {
+        [HarmonyPatch(typeof(SandboxCharacterCreationContent))]
+        [HarmonyPatch("VlandianParentsOnCondition")]
+        public class SandboxCharacterCreationContentVlandianParentsOnConditionPatch
+        {
+            public static void Postfix(ref bool __result, SandboxCharacterCreationContent __instance)
+            {
+                if (__instance.GetSelectedCulture().StringId == "french") __result = true;
+                if (__instance.GetSelectedCulture().StringId == "rekgeist") __result = true;
+            }
+        }
+    }
+}
