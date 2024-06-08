31e9d1b38ee7d2184e692922a039e51bc7f41ba3
Alkoon-mtz <koonmtz@gmail.com>
2024-06-08 02:55:03 +0200

    feat: Rework all equipmentSets for CharCreation + fix Reworkedfacekey

--- bin/cs/ReworkedFacekey.cs
+++ bin/cs/ReworkedFacekey.cs
@@ -5,6 +5,8 @@ using TaleWorlds.CampaignSystem.ViewModelCollection.CharacterCreation;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
 using TaleWorlds.ObjectSystem;
+using System;
+using System.Collections.Generic;
 
 namespace Split
 {
@@ -33,7 +35,30 @@ namespace Split
             // Use default_face_key to set default character of a culture instead of hardcoded one
             public static bool Prefix(CharacterCreationCultureVM selectedCulture)
             {
-                string keyValue = "<BodyProperties version='4' age='25.84' weight='0.5000' build='0.5000' key=\""+ selectedCulture.Culture.BodyPropertiesValue + "\" />";
+                float weight = 0.500f;
+                float build = 0.500f;
+
+                var cultureValues = new Dictionary<string, (float weight, float build)>
+                {
+                    { "aserai", (0.450f, 0.450f) },
+                    { "battania", (0.350f, 0.525f) },
+                    { "iustrium", (0.600f, 0.475f) },
+                    { "empire", (0.450f, 0.375f) },
+                    { "vlandia", (0.300f, 0.600f) },
+                    { "khuzait", (0.400f, 0.500f) },
+                    { "qinguo", (0.450f, 0.375f) },
+                    { "rekgeist", (0.650f, 0.450f) },
+                    { "scarhok", (0.450f, 0.750f) },
+                    { "sturgia", (0.350f, 0.375f) },
+                    { "zulizwe", (0.200f, 0.875f) }
+                };
+
+                if (cultureValues.TryGetValue(selectedCulture.Culture.StringId, out var values))
+                {
+                    weight = values.weight;
+                    build = values.build;
+                }
+                string keyValue = "<BodyProperties version='4' age='25.84' weight='"+weight+"' build='"+build+"' key=\"" + selectedCulture.Culture.BodyPropertiesValue + "\" />";
 
                 BodyProperties properties;
                 if (BodyProperties.FromString(keyValue, out properties))
